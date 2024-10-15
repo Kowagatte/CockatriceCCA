e215757e300e2ad4b9f6d3aa9fa7f19c4fe5a839
Kowagatte <nnryanp@gmail.com>
2024-10-15 12:56:45 -0600

    Added support for archidekt and double-sided cards

--- index.js
+++ index.js
@@ -31,7 +31,17 @@ async function get_from_scryfall(set, num){
 }
 
 function parse_card_line(card){
-    const reg = /(\d)\s(\S.+)\s\((\S.+)\)\s([^\s]+).*/g
+
+    var deck_site = document.querySelector('input[name="deck_site"]:checked').value
+    var reg;
+
+    if(deck_site == "moxfield"){
+        reg = /(\d)\s(\S.+)\s\((\S.+)\)\s([^\s]+).*/g
+    }else if(deck_site == "archidekt"){
+        reg = /(\d)\S\s(\S.+)\s\((\S.+)\)\s([^\s]+).*/g
+    }
+
+    //const reg = /(\d)\s(\S.+)\s\((\S.+)\)\s([^\s]+).*/g
     //return card.replace(reg, '$1').split("/#/")
     const array = [...card.matchAll(reg)]
     array[0].shift()
@@ -39,19 +49,54 @@ function parse_card_line(card){
 }
 
 function parse_to_cock(sj){
-    pt = ""
-    if ("power" in sj){
-        pt = "<pt>" + sj["power"] + "/" + sj["toughness"] + "</pt>\n"
-    }
-    
-    mid = ""
-    if ("multiverse_ids" in sj){
-        if (sj["multiverse_ids"].length > 0){
-            mid = sj["multiverse_ids"][0]
-        }
-    }
 
-    return `
+    console.log(sj)
+    console.log(sj["layout"])
+
+    if(["transform", "modal_dfc", "double_faced_token", "reversible_card"].includes(sj["layout"])){
+        var front = sj["card_faces"]["0"]
+        var back = sj["card_faces"]["1"]
+        return `
+    <card>
+      <name>${front["name"]}</name>
+      <text>${front["oracle_text"]}</text>
+      <prop>
+        <layout>${sj["layout"]}</layout>
+        <side>front</side>
+        <type>${front["type_line"]}</type>
+        <manacost>${front["mana_cost"]}</manacost>
+        <cmc>${sj["cmc"]}</cmc>
+        ${("colors" in front) ? `<colors>${front["colors"].join("")}</colors>\n` : ""}
+        <coloridentity>${sj["color_identity"].join("")}</coloridentity>
+        ${("power" in front) ? `<pt>${front["power"]}/${front["toughness"]}</pt>\n`: ""}
+        <format-standard>${sj["legalities"]["standard"]}</format-standard>
+        <format-commander>${sj["legalities"]["commander"]}</format-commander>
+        <format-modern>${sj["legalities"]["modern"]}</format-modern>
+        <format-pauper>${sj["legalities"]["pauper"]}</format-pauper>
+      </prop>
+      <set rarity="${sj["rarity"]}" uuid="${sj["oracle_id"]}" num="${sj["collector_number"]}" muid="${(("multiverse_ids" in sj) && (sj["multiverse_ids"].length > 0)) ? sj["multiverse_ids"][0] : ""}" picurl="${front["image_uris"]["normal"]}">${deck_name}</set>
+    </card>
+    <card>
+      <name>${back["name"]}</name>
+      <text>${back["oracle_text"]}</text>
+      <prop>
+        <layout>${sj["layout"]}</layout>
+        <side>back</side>
+        <type>${back["type_line"]}</type>
+        <manacost>${back["mana_cost"]}</manacost>
+        ${("colors" in back) ? `<colors>${back["colors"].join("")}</colors>\n` : ""}
+        <coloridentity>${back["color_indicator"].join("")}</coloridentity>
+        ${("power" in back) ? `<pt>${back["power"]}/${back["toughness"]}</pt>\n`: ""}
+        <format-standard>${sj["legalities"]["standard"]}</format-standard>
+        <format-commander>${sj["legalities"]["commander"]}</format-commander>
+        <format-modern>${sj["legalities"]["modern"]}</format-modern>
+        <format-pauper>${sj["legalities"]["pauper"]}</format-pauper>
+      </prop>
+      <set rarity="${sj["rarity"]}" uuid="${sj["oracle_id"]}" num="${sj["collector_number"]}" muid="${(("multiverse_ids" in sj) && (sj["multiverse_ids"].length > 0)) ? sj["multiverse_ids"][0] : ""}" picurl="${back["image_uris"]["normal"]}">${deck_name}</set>
+    </card>`
+
+    }else{
+        return `
     <card>
       <name>${sj["name"]}</name>
       <text>${sj["oracle_text"]}</text>
@@ -61,16 +106,18 @@ function parse_to_cock(sj){
         <type>${sj["type_line"]}</type>
         <manacost>${sj["mana_cost"]}</manacost>
         <cmc>${sj["cmc"]}</cmc>
-        <colors>${sj["colors"].join("")}</colors>
+        ${("colors" in sj) ? `<colors>${sj["colors"].join("")}</colors>\n` : ""}
         <coloridentity>${sj["color_identity"].join("")}</coloridentity>
-        ${pt}
+        ${("power" in sj) ? `<pt>${sj["power"]}/${sj["toughness"]}</pt>\n`: ""}
         <format-standard>${sj["legalities"]["standard"]}</format-standard>
         <format-commander>${sj["legalities"]["commander"]}</format-commander>
         <format-modern>${sj["legalities"]["modern"]}</format-modern>
         <format-pauper>${sj["legalities"]["pauper"]}</format-pauper>
       </prop>
-      <set rarity="${sj["rarity"]}" uuid="${sj["oracle_id"]}" num="${sj["collector_number"]}" muid="${mid}" picurl="${sj["image_uris"]["normal"]}">${deck_name}</set>
+      <set rarity="${sj["rarity"]}" uuid="${sj["oracle_id"]}" num="${sj["collector_number"]}" muid="${(("multiverse_ids" in sj) && (sj["multiverse_ids"].length > 0)) ? sj["multiverse_ids"][0] : ""}" picurl="${sj["image_uris"]["normal"]}">${deck_name}</set>
     </card>`
+    }
+
 }
 
 
